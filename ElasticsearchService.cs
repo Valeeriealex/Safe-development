4ad90a1223b1d8abab481f7c0a6cabaeb8d6f5f9
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-04-09 11:19:50 +0300

    Update ElasticsearchService.cs

--- ElasticsearchService.cs
+++ ElasticsearchService.cs
@@ -1,6 +1,41 @@
-﻿namespace SafeDevelopment
+using Nest;
+using System.Collections.Generic;
+
+namespace SafeDevelopment
 {
     public class ElasticsearchService
     {
+        private readonly ElasticClient _client;
+
+        public ElasticsearchService(string connectionString)
+        {
+            var settings = new ConnectionSettings(new Uri(connectionString));
+            _client = new ElasticClient(settings);
+        }
+
+        public void IndexBook(Book book)
+        {
+            _client.Index(book);
+        }
+
+        public List<Book> SearchBooks(string query)
+        {
+            var searchResponse = _client.Search<Book>(s => s
+                .Query(q => q
+                    .Bool(b => b
+                        .Should(
+                            sh => sh.Match(m => m
+                                .Field(f => f.Title)
+                                .Query(query)),
+                            sh => sh.Match(m => m
+                                .Field(f => f.Description)
+                                .Query(query))
+                        )
+                    )
+                )
+            );
+
+            return searchResponse.Documents.ToList();
+        }
     }
 }
