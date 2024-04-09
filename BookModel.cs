171deb0babeeb4ea5c3c21a9499691eb96f9c539
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-04-09 11:18:55 +0300

    Update BookModel.cs

--- BookModel.cs
+++ BookModel.cs
@@ -2,5 +2,8 @@
 {
     public class BookModel
     {
+         public ObjectId Id { get; set; }
+         public string Title { get; set; }
+         public string Description { get; set; }
     }
 }
