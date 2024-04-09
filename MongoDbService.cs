8ce0df1c47b35e8d9257e931fda6440c1bf3cd0e
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-04-09 11:20:51 +0300

    Update MongoDbService.cs

--- MongoDbService.cs
+++ MongoDbService.cs
@@ -1,6 +1,43 @@
-﻿namespace SafeDevelopment
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace SafeDevelopment
 {
     public class MongoDbService
     {
+        private readonly IMongoCollection<Book> _books;
+
+        public MongoDbService(string connectionString, string databaseName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            _books = database.GetCollection<Book>(collectionName);
+        }
+
+        public Book AddBook(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book GetBook(ObjectId id)
+        {
+            return _books.Find(book => book.Id == id).FirstOrDefault();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _books.ReplaceOne(book => book.Id == book.Id, book);
+        }
+
+        public void DeleteBook(ObjectId id)
+        {
+            _books.DeleteOne(book => book.Id == id);
+        }
     }
 }
